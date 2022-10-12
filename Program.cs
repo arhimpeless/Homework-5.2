2ed2351ddd008a1b60a84d0d69a05442ab6b5c9a
Andrey Markov <arherower@outlook.com>
2022-10-12 09:33:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Задание_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Random rnd = new Random();
+            while (true)
+            {
+                Console.WriteLine("Задайте необходимое количество чисел N. ");
+                Console.Write("Введите N: ");
+                n = int.Parse(Console.ReadLine());
+                List<int> values = new List<int>(n);
+                for (int i = 0; i < n; i++)
+                {
+                    int x = rnd.Next(-1000, 1001);
+                    values.Add(x);
+                }
+                foreach (int i in values)
+                {
+                    Console.Write(i + "\t");
+                }
+                Console.WriteLine("\nMax число: " + values.Max());
+                Console.WriteLine("Min число: " + values.Min() + "\n");
+                Console.WriteLine("\tНажмите Enter для нового цикла.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
